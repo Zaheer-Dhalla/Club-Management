2eafddf49eddfad0a11ccab560b2801d287342cb
Zaheer Dhalla <132003231+Zaheer-Dhalla@users.noreply.github.com>
2024-04-12 17:12:16 -0400

    Create config.js

---
+++ Firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpDdhFHkWYoiS0flQjIPg_qu7eLRVQN7c",
+  authDomain: "club-management-8db80.firebaseapp.com",
+  databaseURL: "https://club-management-8db80-default-rtdb.firebaseio.com",
+  projectId: "club-management-8db80",
+  storageBucket: "club-management-8db80.appspot.com",
+  messagingSenderId: "443048315724",
+  appId: "1:443048315724:web:ca9189e2dea13fd1fb5de9",
+  measurementId: "G-KJ0MBFYR69"
+};
+export {firebaseConfig};
